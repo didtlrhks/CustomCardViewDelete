b9e329df8750df0d21f81eb9f548ba721bbe97f4
sikwan <yysskk99999@naver.com>
2024-03-26 12:15:22 +0900

    init

---
+++ CustomCardDelete_playground/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CustomCardDelete_playground
+//
+//  Created by 양시관 on 3/26/24.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CartView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ CustomCardDelete_playground/CustomCardDelete_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCardDelete_playgroundApp.swift
+//  CustomCardDelete_playground
+//
+//  Created by 양시관 on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomCardDelete_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
